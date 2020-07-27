e5b063f4b0cbfad356a6c3b1a252994630d1d843
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-27 17:06:03 +0900

    Install Pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
-export const join = (req,res) => res.send("Join");
-export const login = (req,res) => res.send("Login");
-export const user = (req,res) => res.send("User Page");
-export const userDetail = (req,res) => res.send("User Detail");
-export const userProfile = (req,res) => res.send("User Profile");
-export const userInfo = (req,res) => res.send("User Information");
-export const editProfile = (req,res) => res.send("Edit Profile");
-export const editInfo = (req,res) => res.send("Edit Information");
-export const changePassword = (req,res) => res.send("Change Password");
+export const join = (req,res) => res.render("Join");
+export const login = (req,res) => res.render("Login");
+export const user = (req,res) => res.render("User Page");
+export const userDetail = (req,res) => res.render("User Detail");
+export const userProfile = (req,res) => res.render("User Profile");
+export const userInfo = (req,res) => res.render("User Information");
+export const editProfile = (req,res) => res.render("Edit Profile");
+export const editInfo = (req,res) => res.render("Edit Information");
+export const changePassword = (req,res) => res.render("Change Password");
