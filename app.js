50b5a07831b6a6d846857612eead5aa4b9487062
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-28 16:08:49 +0900

    Basic Pug Work

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ import bodyParser from "body-parser";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 
 const app = express();
 
+
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.user, userRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
-export const join = (req,res) => res.render("Join");
-export const login = (req,res) => res.render("Login");
-export const user = (req,res) => res.render("User Page");
-export const userDetail = (req,res) => res.render("User Detail");
-export const userProfile = (req,res) => res.render("User Profile");
-export const userInfo = (req,res) => res.render("User Information");
-export const editProfile = (req,res) => res.render("Edit Profile");
-export const editInfo = (req,res) => res.render("Edit Information");
-export const changePassword = (req,res) => res.render("Change Password");
+export const join = (req,res) => res.render("Join", { pageTitle: "Join" });
+export const login = (req,res) => res.render("Login", { pageTitle: "Login" });
+export const user = (req,res) => res.render("User Page", { pageTitle: "User Page" });
+export const userDetail = (req,res) => res.render("User Detail", { pageTitle: "User Detail" });
+export const userProfile = (req,res) => res.render("User Profile", { pageTitle: "User Profile" });
+export const userInfo = (req,res) => res.render("User Information", { pageTitle: "User Information" });
+export const editProfile = (req,res) => res.render("Edit Profile", { pageTitle: "Edit Profile" });
+export const editInfo = (req,res) => res.render("Edit Information", { pageTitle: "Edit Information" });
+export const changePassword = (req,res) => res.render("Change Password", { pageTitle: "Change Password" });
